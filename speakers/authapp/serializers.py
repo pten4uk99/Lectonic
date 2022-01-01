a58d323cc6ea594214106b26cfcb16d9dd82341b
pten4uk <pten4ik99@yandex.ru>
2022-01-01 17:16:03 +0300

    change responses and add some validation on email and password

--- speakers/authapp/serializers.py
+++ speakers/authapp/serializers.py
@@ -1,4 +1,7 @@
+import re
+
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from .models import UserProfile, Token
 
@@ -13,25 +16,40 @@ class UserProfileCreateSerializer(serializers.ModelSerializer):
         )
 
     def validate_u_email(self, email):
-        ''' Валидация эмейла '''
+        '''
+        Валидация эмейла
+
+        1. В строке может быть только один символ '@' и одна '.'
+        2. До '@' могут быть любые буквы, цифры и '.'
+        3. После '@' могут быть любые буквы и цифры
+        4. После точки может быть от 2-х до 4-х латинских маленьких буквы
+        5. В строке не может быть пробелов
+        '''
+
+        match = re.findall(r'^[\w.]+@\w+\.[a-z]{2,4}$', email)
+
+        if not match:
+            raise ValidationError('Некорректный эмейл')
         return email
 
     def validate_u_password(self, password):
-        ''' Валидация пароля '''
+        '''
+        Валидация пароля
+
+        1. Длина пароля от 8 символов
+        '''
+
+        # Для удобства в режиме разработки ограничение пароля только по длине
+        match = re.findall(r'^.{8,}$', password)
+
+        if not match:
+            raise ValidationError('Пароль слишком короткий')
         return password
 
 
 class UserProfileLoginSerializer(serializers.Serializer):
     u_email = serializers.EmailField()
     u_password = serializers.CharField()
-
-    def validate_u_email(self, email):
-        ''' Валидация эмейла '''
-        return email
-
-    def validate_u_password(self, password):
-        ''' Валидация пароля '''
-        return password
 
     def get_object(self):
         ''' Из переданных данных получает объект пользователя '''
