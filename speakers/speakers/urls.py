365c8851b91a139c5d5bc2507f7404d0f547ca97
pten4uk <pten4ik99@yandex.ru>
2022-01-27 00:19:25 +0300

    configure frontend

--- speakers/speakers/urls.py
+++ speakers/speakers/urls.py
@@ -1,12 +1,19 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
 from .utils.yasg import urlpatterns as swagger_urls
 from workroomsapp.views.guest_views import *
+from .views import index
 
 urlpatterns = [
+    path('', index),
+    path('verify_email/', index),
+    path('continue_registration/', index),
+    path('user_info-form/', index),
+
     path('admin/', admin.site.urls),
 
     path('api/auth/', include('authapp.urls')),
@@ -15,4 +22,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += swagger_urls
