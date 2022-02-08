eb4c86c33787229a573c5fb700a21c3e05e21b00
pten4uk <pten4ik99@yandex.ru>
2022-02-08 16:51:31 +0300

    fix bugs

--- speakers/workroomsapp/serializers/person_serializers.py
+++ speakers/workroomsapp/serializers/person_serializers.py
@@ -6,7 +6,53 @@ from rest_framework import serializers
 from workroomsapp.models import Person, City
 
 
-class PersonCreateSerializer(serializers.Serializer):
+class PersonSerializerMixin:
+    def name_validator(self, name):
+        '''
+        Общий валидатор для имени, фамилии и отчества.
+
+        1. Все буквы должны быть кириллицей
+        2. Первая буква имени должна быть заглвной
+        3. Затем может быть сколько угодно строчных символов без пробелов
+
+        '''
+
+        match = re.findall(r'^[A-Я][а-яё-]+$', name)
+
+        if not match:
+            raise serializers.ValidationError('Неверный формат имени')
+
+        return name
+
+    def validate_first_name(self, first_name):
+        return self.name_validator(first_name)
+
+    def validate_last_name(self, last_name):
+        return self.name_validator(last_name)
+
+    def validate_middle_name(self, middle_name):
+        return self.name_validator(middle_name)
+
+    def validate_birth_date(self, birth_date):
+        '''
+        1. Дата не может быть позже сегодняшней
+        '''
+
+        if birth_date > datetime.date.today():
+            raise serializers.ValidationError('Дата не может быть позже текущей')
+
+        return birth_date
+
+    def validate_city(self, city):
+        match = re.findall(r'^[А-Яа-я]+$', city)
+
+        if not match:
+            raise serializers.ValidationError('Неверный формат')
+
+        return city
+
+
+class PersonCreateSerializer(serializers.Serializer, PersonSerializerMixin):
     first_name = serializers.CharField(max_length=100)
     last_name = serializers.CharField(max_length=100)
     middle_name = serializers.CharField(max_length=100, required=False)
@@ -48,52 +94,8 @@ class PersonCreateSerializer(serializers.Serializer):
             'longitude',
         ]
 
-    def name_validator(self, name):
-        '''
-        Общий валидатор для имени, фамилии и отчества.
 
-        1. Все буквы должны быть кириллицей
-        2. Первая буква имени должна быть заглвной
-        3. Затем может быть сколько угодно строчных символов без пробелов
-
-        '''
-
-        match = re.findall(r'^[A-Я][а-я]+$', name)
-
-        if not match:
-            raise serializers.ValidationError('Неверный формат имени')
-
-        return name
-
-    def validate_first_name(self, first_name):
-        return self.name_validator(first_name)
-
-    def validate_last_name(self, last_name):
-        return self.name_validator(last_name)
-
-    def validate_middle_name(self, middle_name):
-        return self.name_validator(middle_name)
-
-    def validate_birth_date(self, birth_date):
-        '''
-        1. Дата не может быть позже сегодняшней
-        '''
-
-        if birth_date > datetime.date.today():
-            raise serializers.ValidationError('Дата не может быть позже текущей')
-
-        return birth_date
-
-    def validate_city(self, city):
-        match = re.findall(r'^[А-Яа-я]+$', city)
-
-        if not match:
-            raise serializers.ValidationError('Неверный формат')
-
-        return city
-
-
-class PersonGetPatchSerializer(serializers.ModelSerializer):
+class PersonGetPatchSerializer(serializers.ModelSerializer, PersonSerializerMixin):
     city = serializers.StringRelatedField()
 
     class Meta:
