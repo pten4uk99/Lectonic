3cdefedc953df082858b58f1ce13f81df2a71d84
Anastasia <g.ankalashik@gmail.com>
2022-03-17 00:46:34 +0400

    Test commit

--- frontend/src/components/Authorization/jsx/ConfirmEmail.jsx
+++ frontend/src/components/Authorization/jsx/ConfirmEmail.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import { useSearchParams, useNavigate } from 'react-router-dom'
+import { useSearchParams, useNavigate, useParams } from 'react-router-dom'
 // import '~/styles/ConfirmEmail.styl'
 import { baseURL } from '~/ProjectConstants'
 
 export default function ConfirmEmail() {
-  const navigate = useNavigate()
+  const {title} = useParams();
+
+  console.log('window.location.search: ', window.location.search)
+  
+ // const navigate = useNavigate()
   // const [confirmationError, setConfirmationError] = useState(false);
 
   //используем хук из react-router-dom, чтоб вытащить значение ключа query параметра в url
-  const [searchParams, setSearchParams] = useSearchParams()
-  const emailToken = searchParams.get('key')
-  console.log('emailToken: ', emailToken)
+ const [searchParams, setSearchParams] = useSearchParams()
+ const emailToken = searchParams.get('key')
+ console.log('emailToken: ', emailToken)
 
   //отправляем запрос GET при загрузке страницы
-  fetch(`${baseURL}/api/email/email_confirmation/?key=${emailToken}`, {
+ /*fetch(`${baseURL}/api/email/email_confirmation/?key=${emailToken}`, {
     method: 'GET',
     credentials: 'include',
   })
@@ -32,10 +36,13 @@ export default function ConfirmEmail() {
     })
     .catch(error => {
       console.log('ERROR: ', error)
-    })
+    })*/
 
   return (
     <div>
+      <h1>
+        TestTestTest
+      </h1>
       {/* confirmationError &&
                 <div>
                     <h3>Произошла ошибка, вернитесь в почту и перейдите по ссылке ещё раз</h3>
