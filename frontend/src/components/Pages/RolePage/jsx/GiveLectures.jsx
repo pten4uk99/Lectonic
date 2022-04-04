8797e337f61167cbd65288543d87c250d6dc1f02
Nikita <nikitasar64@gmail.com>
2022-04-04 12:55:04 +0300

    created RolePage component

---
+++ frontend/src/components/Pages/RolePage/jsx/GiveLectures.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import WoorkroomCard from '~@/WorkRooms/WorkRoom/jsx/WorkroomCard';
+import '../styles/GiveLectures.styl'
+
+
+function GiveLectures(props) {
+  return (
+    <>
+        <div className='rolepage__grid'>
+        <WoorkroomCard data={{
+                img: '',
+                client: "Заказчик:",
+                clientName: "ОАО «Какое-то очень длинное название",
+                name: 'Что-то интересное в две строчки, не бопппппп',
+                description: 'Лекции от известных учёных о самом актуальном, но не больше трёх строчек...',
+                textBtn: 'Статус мероприятия',
+                date: "20.03.2022 – 25.03.20224444444",
+                city: "г. Санкт-Петербург",
+                potentialLecture: true,
+            }}/>
+        <WoorkroomCard data={{
+                img: '',
+                client: "Заказчик:",
+                clientName: "ОАО «Какое-то очень длинное название",
+                name: 'Что-то интересное в две строчки, не бопппппп',
+                description: 'Лекции от известных учёных о самом актуальном, но не больше трёх строчек...',
+                textBtn: 'Статус мероприятия',
+                date: "20.03.2022 – 25.03.20224444444",
+                city: "г. Санкт-Петербург",
+                potentialLecture: true,
+            }}/>
+        </div>
+    </>
+  )
+}
+
+export default GiveLectures;
