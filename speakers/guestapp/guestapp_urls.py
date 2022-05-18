9d52735aee14069da9f3507df9493bc4d1223f3c
pten4uk <pten4ik99@yandex.ru>
2022-05-18 17:47:17 +0300

    refactor tests

--- speakers/guestapp/guestapp_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .guestapp_views import LecturesAPIView, LecturersAPIView
-
-urlpatterns = [
-    path('lecture/', LecturesAPIView.as_view()),
-    path('lecturer/', LecturersAPIView.as_view()),
-]
--- speakers/workroomsapp/person/tests/test_person_get.py
+++ speakers/workroomsapp/person/tests/test_person_get.py
@@ -1,22 +1,11 @@
 from django.urls import reverse
-from rest_framework.test import APITestCase
 
-from speakers.utils.tests import data
-from workroomsapp.models import City
+from workroomsapp.person.tests.base import PersonCreateTestCase
 
 
-class TestPersonGet(APITestCase):
-    signup_data = data.SIGNUP.copy()
-    profile_data = data.PROFILE.copy()
-    profile_data['city'] = '1'
-
+class TestPersonGet(PersonCreateTestCase):
     def setUp(self):
-        temp_signup_data = self.signup_data.copy()
-        temp_profile_data = self.profile_data.copy()
-        self.client.post(reverse('signup'), temp_signup_data)
-
-        City.objects.create(name='Москва', pk=1)
-        self.client.post(reverse('profile'), temp_profile_data)
+        super().setUp()
 
     def test_credentials(self):
         self.client.get(reverse('logout'))
